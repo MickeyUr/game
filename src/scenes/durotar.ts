0008bd7df981f83428e22157b3689ced198ed0dc
mickey <michail.uryvalkin@viaduct.pro>
2024-06-26 14:11:36 +0300

    added character and animation

--- src/scenes/durotar.ts
+++ src/scenes/durotar.ts
@@ -1,1 +1,33 @@
-export class Durotar {}
+import Phaser from "phaser";
+import durotarJson from  '../assets/durotar.json'
+import {LAYERS, SIZES, SPRITES, TILES} from "../utils/constants.ts";
+import {Player} from "../entities/player.ts";
+export class Durotar extends Phaser.Scene {
+
+    private player?: Player;
+    constructor() {
+        super('DurotarScene');
+    }
+
+    preload () {
+        this.load.image(TILES.DUROTAR, 'src/assets/Tilemaps/durotar.png')
+        this.load.tilemapTiledJSON('map', 'src/assets/durotar.json')
+        this.load.spritesheet(SPRITES.PLAYER, 'src/assets/characters/alliance.png', {
+            frameWidth: SIZES.PLAYER.WIDTH,
+            frameHeight: SIZES.PLAYER.HEIGHT
+        })
+    }
+
+    create () {
+        const map = this.make.tilemap({key: 'map',} );
+        const tileset  = map.addTilesetImage(durotarJson.tilesets[0].name, TILES.DUROTAR, SIZES.TILE, SIZES.TILE )
+        const groundLayer = map.createLayer(LAYERS.GROUND, tileset, 0 , 0)
+        const wallLayer = map.createLayer(LAYERS.WALLS, tileset, 0 , 0)
+
+        this.player = new Player(this, 400, 250, SPRITES.PLAYER);
+    }
+
+    update(_: number, delta: number): void {
+        this.player.update(delta);
+    }
+}
